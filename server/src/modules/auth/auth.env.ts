7aaa0703ebd62ef9282d0ef995382cc93ce3acd0
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-02-10 23:50:12 +0530

    feat(auth): add env file for loading env variable

---
+++ server/src/modules/auth/auth.env.ts
@@ -0,0 +1,19 @@
+ const requiredEnv = [
+  "ACCESS_TOKEN_SECRET",
+  "REFRESH_TOKEN_SECRET",
+  "ACCESS_TOKEN_EXPIRES",
+  "REFRESH_TOKEN_EXPIRES"
+] as const;
+
+requiredEnv.forEach((key) => {
+  if (!process.env[key]) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+});
+
+export const env = {
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET!,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET!,
+  ACCESS_TOKEN_EXPIRES: Number(process.env.ACCESS_TOKEN_REFRESH),
+  REFRESH_TOKEN_EXPIRES: Number(process.env.REFRESH_TOKEN_EXPIRES)
+};
