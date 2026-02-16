862669c9fc8cbf640dbe31178ff161a2b97a28f6
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-02-16 23:47:44 +0530

    refactor(index): properly configure index file and remove test logs

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from 'express';
 import cors from "cors";
-
+import mainRouter from './routes/index.js'
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(_,res)=>{res.json({status:200,msg:"healthy"})});
-
 const PORT = process.env.PORT 
+app.use("/v1",mainRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
