4e0e9b3b66b4821084722a015c0a0a4df32c835d
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-02-24 05:01:53 +0530

    feat(routes): implement asynchandler for controllers

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -3,12 +3,13 @@ import { loginController, logoutController, signupController } from "./auth.cont
 import { validate } from "../../middlewares/validate.middleware";
 import { loginSchema, signupSchema } from "./auth.schema";
 import { refreshService } from "./auth.refresh";
+import { asyncHandler } from "../../utils/asyncHandler";
 
 
 const router = Router();
 
-router.post('/signup',validate(signupSchema),signupController);
-router.post('/login',validate(loginSchema),loginController);
-router.post('/logout',logoutController);
+router.post('/signup',validate(signupSchema),asyncHandler(signupController));
+router.post('/login',validate(loginSchema),asyncHandler(loginController));
+router.post('/logout',asyncHandler(logoutController));
 router.post('/refresh',refreshService);
 export default router;
