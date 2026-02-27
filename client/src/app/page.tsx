4cde428faebeaeaeb15691b9a93387a3deb6dcfc
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-02-28 00:48:42 +0530

    initial commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="bg-black flex items-center justify-center min-h-screen">
+        <h1>Yash Mishra working online</h1>
+      </main>
+    </div>
+  );
+}
