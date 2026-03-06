0ae5e8356bf4f39da069d4bea3c890be76b4822f
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-03-06 23:41:04 +0530

    feat(routes): add global error handler error middleware

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import authRouter from '../modules/auth/auth.routes';
+import { globalErrorHandler } from "../middlewares/globalErrorHandler.middleware";
 
 const router = Router();
 
-router.use("/auth",authRouter)
-
+router.use("/auth",authRouter);
+router.use(globalErrorHandler);
 export default router;
