820a2a20679116b8030ec773ab637098f0d8273c
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-04-24 00:00:47 +0530

    refactor(middleware): change response status code and req type

--- server/src/middlewares/authorize.middleware.ts
+++ server/src/middlewares/authorize.middleware.ts
@@ -1,14 +1,13 @@
-import { NextFunction, Response } from "express";
-import { AuthenticateType } from "../types/authenticateType";
+import { NextFunction, Response, Request } from "express";
 import { Role } from "../types/authorizeRolesType";
 
-export const authorize = (...allowedRoles:Role[]) => ( req:AuthenticateType,res:Response,next:NextFunction ) => {
+export const authorize = (...allowedRoles:Role[]) => ( req:Request,res:Response,next:NextFunction ) => {
  if(!req.user){
     return res.status(401).json({message:"Unauthorized"});
  }
 
  if(!allowedRoles.includes(req.user.role as Role)){
-     return res.status(401).json({message:"Forbidden"});
+     return res.status(403).json({message:"Forbidden"});
  }
 
  next();
